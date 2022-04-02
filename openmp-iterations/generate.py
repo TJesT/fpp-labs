86ed790bf3b982bb94e5b76c68e4c4377d86c34b
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2022-04-02 12:29:31 +0700

    Update generate.py

--- openmp-iterations/generate.py
+++ openmp-iterations/generate.py
@@ -1,15 +1,13 @@
-from random import randint as ri
+from random import random
 
-n = 200
+n = 1000
 
 m = [[0]*n for i in range(n)]
 b = [0]*n
 
-x = 1
-
 def gen(m,b, n, fwithargs):
     for i in range(n):
-        m[i][i] = str(fwithargs[0](*fwithargs[1:]))
+        m[i][i] = str(fwithargs[0](*fwithargs[1:]) + n)
         for j in range(i + 1, n):
             m[i][j] = str(fwithargs[0](*fwithargs[1:]))
             m[j][i] = m[i][j]
@@ -17,16 +15,8 @@ def gen(m,b, n, fwithargs):
     for i in range(n):
         b[i] = str(fwithargs[0](*fwithargs[1:]))
 
-def oneortwo():
-    global x
-    if x == 1:
-        x = 2
-    else:
-        x = 1
-    return x
-
-gen(m, b, n, (ri, -20, 20))
-# gen(m, b, n, (oneortwo,))
+# gen(m, b, n, (ri, -20, 20))
+gen(m, b, n, (random,))
 
 with open("input.txt", "w") as inp:
     inp.write(f"{n}\n")
